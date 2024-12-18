db13d21aacb9167a81d698d65fa377ffcdc177b4
Jesse <jesseabe.rodrigues@gmail.com>
2024-12-18 09:06:53 -0300

    Day 01: Done

---
+++ 01-Day/taks.py
@@ -0,0 +1,25 @@
+# How to print in different lines using the same print:
+print("Test \nTest\nTest")
+
+#Combine 
+print("Test" + " " + "Testing")
+
+#Function Input
+input("What is your name?")
+
+#Funciont for collect and print the name
+print("Hello " + input("What is your name?") + "!test")
+
+#Python variables
+name = input("What is your name?")
+print(name)
+
+#Counting the number of characters 
+a = "Hello, world!"
+print(len(a))
+
+print(len(input("What's your name? ")))
+
+name = input("What is your name?")
+length = len(name)
+print(length)
