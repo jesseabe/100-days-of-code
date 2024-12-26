30a4a1b6ad3ba918e9b2720905771438f0ba9004
Jesse <jesseabe.rodrigues@gmail.com>
2024-12-26 14:47:06 -0300

    Day 09: Done

---
+++ 09-Day/bidding.py
@@ -0,0 +1,31 @@
+print("Hello")
+
+def find_highest_bidder(bidding_dictionary):
+    winner = ""
+    highest_bid = 0
+
+    for bidder in bidding_dictionary:
+        bid_amount = bidding_dictionary[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    
+    print(f"The winner is {winner} with a bid of $ {highest_bid}")
+
+
+bids = {}
+contiue_bidding = True
+while contiue_bidding: 
+    name = input("What's your name? ")
+    price = int(input("What's your bid? $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no'. \n")
+    if should_continue == "no":
+        contiue_bidding = False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        print("\n" * 20 )
+
+
+
+
