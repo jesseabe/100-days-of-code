efdc9f70be1a0d77120ff8790c4395b49fa7a7f0
Jesse <jesseabe.rodrigues@gmail.com>
2024-12-31 08:02:22 -0300

    Day 14: Done

---
+++ 14-Day/game_data.py
@@ -0,0 +1,122 @@
+data = [
+    {
+        'name': 'Instagram',
+        'follower_count': 346,
+        'description': 'Social media platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 600,
+        'description': 'Footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Lionel Messi',
+        'follower_count': 490,
+        'description': 'Footballer',
+        'country': 'Argentina'
+    },
+    {
+        'name': 'Kim Kardashian',
+        'follower_count': 360,
+        'description': 'Reality TV personality and businesswoman',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kylie Jenner',
+        'follower_count': 400,
+        'description': 'Reality TV personality and businesswoman',
+        'country': 'United States'
+    },
+    {
+        'name': 'Nike',
+        'follower_count': 280,
+        'description': 'Sportswear brand',
+        'country': 'United States'
+    },
+    {
+        'name': 'Dwayne Johnson',
+        'follower_count': 390,
+        'description': 'Actor and former professional wrestler',
+        'country': 'United States'
+    },
+    {
+        'name': 'Selena Gomez',
+        'follower_count': 420,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Beyoncé',
+        'follower_count': 315,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Justin Bieber',
+        'follower_count': 380,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Taylor Swift',
+        'follower_count': 310,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'LeBron James',
+        'follower_count': 160,
+        'description': 'Basketball player',
+        'country': 'United States'
+    },
+    {
+        'name': 'Adidas',
+        'follower_count': 110,
+        'description': 'Sportswear brand',
+        'country': 'Germany'
+    },
+    {
+        'name': 'Billie Eilish',
+        'follower_count': 110,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Shakira',
+        'follower_count': 90,
+        'description': 'Musician',
+        'country': 'Colombia'
+    },
+    {
+        'name': 'Rihanna',
+        'follower_count': 140,
+        'description': 'Musician and businesswoman',
+        'country': 'Barbados'
+    },
+    {
+        'name': 'Zendaya',
+        'follower_count': 190,
+        'description': 'Actress and singer',
+        'country': 'United States'
+    },
+    {
+        'name': 'Puma',
+        'follower_count': 20,
+        'description': 'Sportswear brand',
+        'country': 'Germany'
+    },
+    {
+        'name': 'Drake',
+        'follower_count': 120,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Netflix',
+        'follower_count': 45,
+        'description': 'Streaming platform',
+        'country': 'United States'
+    }
+]
